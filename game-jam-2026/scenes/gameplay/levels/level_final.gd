20b4e58b2908e1b1c0c15f9757b5b8e54b6c8c24
jailynyun <jailynyun@gmail.com>
2026-02-01 13:19:57 -0800

    final level win screen

--- game-jam-2026/scenes/gameplay/levels/level_final.gd
+++ game-jam-2026/scenes/gameplay/levels/level_final.gd
@@ -1,10 +1,32 @@
 extends Node2D
 
-@onready var interactable: Area2D = $mirror/Interactable
-#@onready var fade: CanvasLayer = $Fade
+@onready var mirror_interactable: Area2D = $mirror/Interactable
+@onready var mirror_shape: CollisionShape2D = $mirror/Interactable/CollisionShape2D
+
+@onready var podiums := [
+	$podium1,
+	$podium2,
+	$podium3
+]
+
+var podiums_completed := 0
 
 func _ready() -> void:
-	interactable.interact = _on_interact
+	# Disable mirror at start
+	_set_mirror_enabled(false)
 
-func _on_interact():
-	pass
+	# Connect each podium's signal
+	for p in podiums:
+		p.podium_placed.connect(_on_podium_placed)
+
+func _on_podium_placed() -> void:
+	podiums_completed += 1
+	print("Podiums completed:", podiums_completed)
+
+	if podiums_completed >= 3:
+		_set_mirror_enabled(true)
+
+func _set_mirror_enabled(enabled: bool) -> void:
+	mirror_interactable.monitoring = enabled
+	mirror_interactable.monitorable = enabled
+	mirror_shape.disabled = !enabled
