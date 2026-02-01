c0bdcde833af6e8f3725426d74ae3bf85e3b9b84
jailynyun <jailynyun@gmail.com>
2026-01-31 16:11:17 -0800

    running animation

--- game-jam-2026/scenes/gameplay/player.gd
+++ game-jam-2026/scenes/gameplay/player.gd
@@ -1,13 +1,20 @@
 extends CharacterBody2D
 
-const SPEED = 300
-
+const SPEED = 50
+@onready var animated_sprite = $AnimatedSprite2D
+@warning_ignore("unused_parameter")
 func _physics_process(delta):
 	var direction = Input.get_vector("left", "right", "up", "down")
-	if direction:
-		velocity = direction  * SPEED
+	if direction != Vector2.ZERO:
+		velocity = direction * SPEED
+
+		# Flip based on horizontal direction
+		if direction.x != 0:
+			animated_sprite.flip_h = direction.x < 0
+
+		animated_sprite.play("walk_right")
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.y = move_toward(velocity.y, 0, SPEED)
-		
+		velocity = velocity.move_toward(Vector2.ZERO, SPEED)
+		animated_sprite.play("idle_right")
+
 	move_and_slide()
