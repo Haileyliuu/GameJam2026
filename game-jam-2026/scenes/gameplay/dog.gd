852a6d9240a3ef1fa8190ce437b3083e43241f78
isabelleLi-Projects <iggiesports10@gmail.com>
2026-02-01 14:38:47 -0800

    added dog sound

--- game-jam-2026/scenes/gameplay/dog.gd
+++ game-jam-2026/scenes/gameplay/dog.gd
@@ -1,11 +1,22 @@
 extends Sprite2D
 
 var triggered := false
+@onready var audio_stream_player_2d: AudioStreamPlayer2D = $AudioStreamPlayer2D
 
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
+	
 	if triggered:
 		return
 	if body.is_in_group("Player"):
 		triggered = true
+
+		var p := $AudioStreamPlayer
+		remove_child(p)
+		get_tree().current_scene.add_child(p)
+		#p.global_position = global_position
+		p.play()
+
 		get_tree().call_group("Enemies", "start_chase", body)
+
+		queue_free()
