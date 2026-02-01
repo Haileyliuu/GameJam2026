69d50edbc05b6ff84ed2722234ae09dbb13d22e8
isabelleLi-Projects <iggiesports10@gmail.com>
2026-02-01 14:42:37 -0800

    dog sound+

--- game-jam-2026/scenes/gameplay/dog.gd
+++ game-jam-2026/scenes/gameplay/dog.gd
@@ -15,8 +15,9 @@ func _on_area_2d_body_entered(body: Node2D) -> void:
 		remove_child(p)
 		get_tree().current_scene.add_child(p)
 		#p.global_position = global_position
-		p.play()
+		
 
 		get_tree().call_group("Enemies", "start_chase", body)
 
 		queue_free()
+		p.play()
